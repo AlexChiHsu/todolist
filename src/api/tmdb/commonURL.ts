b0137ab6bd29e8ffaada2a235222413862ffdef2
Alex Hsu <smilechi1993@gmail.com>
2023-08-10 08:49:20 +0800

    apiurl

---
+++ src/api/tmdb/commonURL.ts
@@ -0,0 +1,8 @@
+export const imageURL = "https://image.tmdb.org/t/p/original";
+export const apiURL = "https://api.themoviedb.org/3";
+export const apiKey = "901b4e4633acf0c730a8f8d95a917388";
+export const headers = {
+  accept: "application/json",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDFiNGU0NjMzYWNmMGM3MzBhOGY4ZDk1YTkxNzM4OCIsInN1YiI6IjY0ZDM1N2Y0MDM3MjY0MDBlMmJlYTIzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QABu3_YFeZ9avp1OGnhLoJn9AqpoASV5ooMwxEi476U",
+};
