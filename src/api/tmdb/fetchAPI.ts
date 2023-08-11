3e0b6e1a0b01d667e933be9cfc55b2b32da1a4bd
Alex Hsu <smilechi1993@gmail.com>
2023-08-11 17:28:38 +0800

    api url

--- src/api/tmdb/fetchAPI.ts
+++ src/api/tmdb/fetchAPI.ts
@@ -5,6 +5,8 @@ const URL = {
   GET_POPULAR_TV_LIST:
     "/discover/tv?include_adult=false&include_null_first_air_dates=false&language=zh-TW&page=1&sort_by=popularity.desc&with_origin_country={country}&with_original_language={language}",
   GET_POPULAR_TV_ANIMATION_LIST: "/discover/tv?with_genres=16",
+  GET_MOVIE_TOP_RATED: "/movie/top_rated?language=zh-TW",
+  GET_TV_TOP_RATED: "/tv/top_rated?language=zh-TW",
 };
 const options = {
   method: "GET",
@@ -38,6 +40,22 @@ export function getPopularTVList(language: string, country: string) {
 
 export function getPopularTVAnimationList() {
   const fullURL = apiURL + URL.GET_POPULAR_TV_ANIMATION_LIST;
+  return {
+    fullURL,
+    options,
+  };
+}
+
+export function getMovieTopRated() {
+  const fullURL = apiURL + URL.GET_MOVIE_TOP_RATED;
+  return {
+    fullURL,
+    options,
+  };
+}
+
+export function getTvTopRated() {
+  const fullURL = apiURL + URL.GET_TV_TOP_RATED;
   return {
     fullURL,
     options,
